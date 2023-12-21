1a90e0b2ad9040c475eb518e6758d39cecd7d6f3
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-12-21 15:32:47 -0500

    Latest

---
+++ tests/test_mutation.py
@@ -0,0 +1,37 @@
+import pytest
+from egp_physics.mutation import gc_remove, gc_remove_all_connections, gc_add_input, gc_remove_input, gc_add_output, gc_remove_output, gc_remove_constant
+
+# Test gc_remove function
+def test_gc_remove():
+    # Add your test cases here
+    pass
+
+# Test gc_remove_all_connections function
+def test_gc_remove_all_connections():
+    # Add your test cases here
+    pass
+
+# Test gc_add_input function
+def test_gc_add_input():
+    # Add your test cases here
+    pass
+
+# Test gc_remove_input function
+def test_gc_remove_input():
+    # Add your test cases here
+    pass
+
+# Test gc_add_output function
+def test_gc_add_output():
+    # Add your test cases here
+    pass
+
+# Test gc_remove_output function
+def test_gc_remove_output():
+    # Add your test cases here
+    pass
+
+# Test gc_remove_constant function
+def test_gc_remove_constant():
+    # Add your test cases here
+    pass
